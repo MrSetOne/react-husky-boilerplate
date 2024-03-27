cd8729fcc2bf209d7166e29b2ce51405341de160
Michael Lara Sánchez <lara.sanchez.michael@gmail.com>
2024-03-27 19:58:56 +0100

    chore: :construction: wip

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,6 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'revert']],
-    'scope-empty': [2, 'never'],
-    'subject-empty': [2, 'never'],
-    'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 72],
+    'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'revert', 'wip']],
   },
 }
