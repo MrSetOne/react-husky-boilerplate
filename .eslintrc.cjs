917dbff835e9fee77e296a736b3769f75e0858f2
Michael Lara Sánchez <lara.sanchez.michael@gmail.com>
2024-03-27 19:58:25 +0100

    chore: :construction: wip

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules', 'package.json'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    eqeqeq: ['error', 'always'],
+    '@typescript-eslint/no-inferrable-types': 'off',
+    'no-empty-function': 'error',
+    'no-unused-vars': 'error',
+    'no-var': 'error',
+    'prefer-const': 'warn',
+  },
+}
